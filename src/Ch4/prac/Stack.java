d0950ca56f587a2cce0e24700fc76d56bf4f8656
pipi-shortstocking <jannie435@naver.com>
2024-06-14 16:47:46 +0900

    no.2 진행 중

---
+++ src/Ch4/prac/Stack.java
@@ -0,0 +1,11 @@
+package Ch4.prac;
+
+public class Stack<E> {
+    private E [] stk;
+    private int capacity;
+    private int ptr;
+
+    // 실행 시 예외
+    // 스택이 비어 있음
+
+}
---
+++ src/Ch4/prac/no2.java
@@ -0,0 +1,7 @@
+package Ch4.prac;
+
+public class no2 {
+    public static void main(String[] args) {
+
+    }
+}
